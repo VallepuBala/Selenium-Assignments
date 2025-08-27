c0ddb066a1747ae1a0e455b49b6911d121b06855
VallepuBala <baladurgeshv1@gmail.com>
2025-08-27 14:38:48 +0530

    practice

--- src/SeleniumPractice/LinkTesting.java
+++ src/SeleniumPractice/LinkTesting.java
@@ -8,10 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkTesting {
-
-	public static void main(String[] args) throws InterruptedException  {
-		
-		WebDriver driver = new ChromeDriver();
+	
+	public static void borniGarciaTesting(WebDriver driver) throws InterruptedException {
 		driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
 		driver.manage().window().maximize();
 		
@@ -21,12 +19,45 @@ public class LinkTesting {
 		for(WebElement l :list) {
 			System.out.println("Text of link: " + l.getText());
 			System.out.println("href of link: " + l.getDomAttribute("href"));
+			System.out.println();
 			l.click();
 			Thread.sleep(3000);
 			driver.navigate().back();
 		}
 		
 		driver.quit();
+	}
+	
+	public static void amazonLinksTesting(WebDriver driver) throws InterruptedException {
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		WebElement divOfLinks = driver.findElement(By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[5]"));
+		List<WebElement> list = divOfLinks.findElements(By.tagName("a"));
+		
+		for(int i = 0; i <= list.size()-1; i++) {
+			WebElement link = list.get(i);
+			link.click();
+			Thread.sleep(2000);
+			driver.navigate().back();
+			
+			Thread.sleep(2000);
+			divOfLinks = driver.findElement(By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[5]"));
+			list = divOfLinks.findElements(By.tagName("a"));
+			
+		}
+		driver.quit();
+		
+	}
+
+	
+	public static void main(String[] args) throws InterruptedException  {
+		
+		WebDriver driver = new ChromeDriver();
+		borniGarciaTesting(driver);
+		amazonLinksTesting(driver);
+		
+		
 
 	}
 
